5baa03c8fc617e86b5da9b4dc237d8cd736ff98a
SeminVsevolod <semin.vsevolod@gmail.com>
2020-08-07 19:07:14 +0300

    вызов таблицы с моками из домашней страницы

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2 class="text-3xl">Товары</h2>
+    <Table :rows="rows" :columns="columns" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Table from "@/components/table/Table.vue";
+import TableTypes from "@/components/table/TableTypes";
 
 export default {
   name: "Home",
+
   components: {
-    HelloWorld
+    Table
+  },
+
+  data() {
+    return {
+      rows: [
+        {
+          name: "Манго",
+          price: "200 руб",
+          totalCell: "73"
+        },
+        {
+          name: "Яблоко",
+          price: "75 руб",
+          totalCell: "865"
+        },
+        {
+          name: "Апельсины",
+          price: "60 руб",
+          totalCell: "341"
+        }
+      ],
+
+      columns: [
+        {
+          type: TableTypes.String,
+          key: "name",
+          label: "Название товара"
+        },
+        {
+          type: TableTypes.Number,
+          key: "price",
+          label: "Цена"
+        },
+        {
+          type: TableTypes.Number,
+          key: "totalCell",
+          label: "Продаж за месяц"
+        }
+      ]
+    };
   }
 };
 </script>
